e7b24c9dcfbe6d8c71cec688856d4ef457720b3d
Geert Eltink <dev@elt.ink>
2020-12-18 18:13:43 +0100

    fix: only set display env in wsl

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -2,5 +2,8 @@ export UID
 export HOST_GID=$(id -g)
 export HOST_UID=$(id -u)
 
-export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
-export LIBGL_ALWAYS_INDIRECT=0
+# Only connect to a X server in WSL
+if [[ $(uname -r) =~ (M|m)icrosoft ]]; then
+    export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
+    export LIBGL_ALWAYS_INDIRECT=0
+fi
