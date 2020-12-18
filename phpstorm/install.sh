7ca2422bc2f27affac99676b4f714d4f45c778c9
Geert Eltink <dev@elt.ink>
2020-12-18 18:52:03 +0100

    feat: add phpstorm install script

---
+++ phpstorm/install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+if [ -d /opt/phpstorm ]; then
+   exit 0
+fi
+
+if [ ! -f /tmp/PhpStorm.tar.gz ]; then
+    wget -O /tmp/PhpStorm.tar.gz https://download.jetbrains.com/webide/PhpStorm-2020.3.tar.gz
+fi
+
+tar -xzf /tmp/PhpStorm.tar.gz -C /tmp
+sudo rm /tmp/PhpStorm.tar.gz
+
+for dir in /tmp/PhpStorm-*/
+do
+    sudo rm -rf /opt/phpstorm/
+    sudo mv ${dir} /opt/phpstorm/
+    sudo rm -rf ${dir}
+    sudo ln -nsf /opt/phpstorm/bin/phpstorm.sh /usr/local/bin/phpstorm
+done
+
+exit 0
