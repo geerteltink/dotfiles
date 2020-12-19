aba55e0d556f3be2410eced1e6afa9e20f1c3a71
Geert Eltink <dev@elt.ink>
2020-12-19 19:23:10 +0100

    feat: save history

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -7,3 +7,7 @@ if [[ $(uname -r) =~ (M|m)icrosoft ]]; then
     export DISPLAY=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
     export LIBGL_ALWAYS_INDIRECT=0
 fi
+
+export SAVEHIST=5000
+export HISTSIZE=2000
+export HISTFILE="$HOME/.zsh_history"
