b946ed96d091dc479ff007512fee5ddbec3f7cd9
Geert Eltink <geert.eltink@gmail.com>
2022-09-30 17:45:56 +0200

    feat: remove obsolete zsh package

--- zsh/install.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -Eueo pipefail
-
-[ $(which zsh) ] || sudo apt -y install zsh
-
-ln -nsf $PWD/zsh/zshrc ~/.zshrc
-
-exit 0
--- zsh/path.zsh
+++
@@ -1,5 +0,0 @@
-path=("$HOME/.dotfiles/bin" $path)
-path=("$HOME/bin" $path)
-path=("/usr/local/bin" $path)
-
-export PATH
