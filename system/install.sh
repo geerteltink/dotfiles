9c2b5f8b313fae74d4b4a366e769610704ed2b78
Geert Eltink <geert.eltink@gmail.com>
2023-02-02 08:43:03 +0100

    feat: add automatic time update

--- system/install.sh
+++ system/install.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -Eueo pipefail
 
+PACKAGES=(
+  ntp
+  ntpdate
+)
+
+for pkg in "${PACKAGES[@]}"; do
+  dpkg -s $pkg > /dev/null 2>&1 || sudo apt -y install $pkg
+done
+
+sudo ntpdate ntp.ubuntu.com
 sudo sysctl -w fs.inotify.max_user_watches=524288
 
 exit 0
