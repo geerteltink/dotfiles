6075c65f365c83643f1df2797067919a072281ed
Geert Eltink <geert.eltink@gmail.com>
2023-04-12 07:27:38 +0200

    fix: update time even when in use

--- system/install.sh
+++ system/install.sh
@@ -10,7 +10,7 @@ for pkg in "${PACKAGES[@]}"; do
   dpkg -s $pkg > /dev/null 2>&1 || sudo apt -y install $pkg
 done
 
-sudo ntpdate ntp.ubuntu.com
+sudo ntpdate -u pool.ntp.org
 sudo sysctl -w fs.inotify.max_user_watches=524288
 
 exit 0
