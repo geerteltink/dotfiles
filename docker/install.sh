04f6e6fe9a421dd66ad4c9d5adc8553cfb5b738e
Geert Eltink <geert.eltink@gmail.com>
2023-04-12 07:04:28 +0200

    feat: install docker buildx

--- docker/install.sh
+++ docker/install.sh
@@ -4,7 +4,7 @@ set -Eueo pipefail
 if [ ! -f /etc/apt/sources.list.d/docker.list ]
 then
   # Remove old packages
-  sudo apt remove docker docker.io containerd runc
+  sudo apt remove docker docker-engine docker-ce docker.io containerd runc
 
   # Install dependencies
   PACKAGES=(
@@ -19,23 +19,24 @@ then
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt update
-
-  # Install docker
-  DOCKER_PACKAGES=(
-    docker-ce
-    docker-ce-cli
-    containerd.io
-    docker-compose-plugin
-  )
-
-  for pkg in "${DOCKER_PACKAGES[@]}"; do
-    dpkg -s $pkg > /dev/null 2>&1 || sudo apt -y install $pkg
-  done
-
-  # Add current user to docker group
-  sudo groupadd docker --force
-  sudo usermod -aG docker $USER
 fi
+
+# Install docker
+DOCKER_PACKAGES=(
+  docker-ce
+  docker-ce-cli
+  containerd.io
+  docker-buildx-plugin
+  docker-compose-plugin
+)
+
+for pkg in "${DOCKER_PACKAGES[@]}"; do
+  dpkg -s $pkg > /dev/null 2>&1 || sudo apt -y install $pkg
+done
+
+# Add current user to docker group
+sudo groupadd docker --force
+sudo usermod -aG docker $USER
 
 if [ ! -f /usr/local/bin/docker-compose ]
 then
@@ -54,6 +55,7 @@ if [[ ! -f /etc/docker/daemon.json ]]; then
 fi
 sudo jq -s add /etc/docker/daemon.json $PWD/docker/daemon.json
 
+sudo crudini --set /etc/wsl.conf boot systemd '"true"'
 sudo crudini --set /etc/wsl.conf boot command '"service docker start"'
 
 exit 0
