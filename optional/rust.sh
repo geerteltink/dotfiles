0051600155b1e322285c032b84e38d7d49d11fec
Geert Eltink <geert.eltink@gmail.com>
2023-06-08 17:23:04 +0200

    feat: install gcc with rust

--- optional/rust.sh
+++ optional/rust.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -Eueo pipefail
 
+PACKAGES=(
+  gcc
+)
+
+for pkg in "${PACKAGES[@]}"; do
+  dpkg -s $pkg > /dev/null 2>&1 || sudo apt -y install $pkg
+done
+
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 
 exit 0
