d2be2224d49b18e2eada821d760988c0cf26dba8
Geert Eltink <geert.eltink@gmail.com>
2023-09-20 13:59:28 +0200

    feat: generate missing locale

--- system/install.sh
+++ system/install.sh
@@ -13,6 +13,11 @@ done
 sudo ntpdate -u pool.ntp.org
 sudo sysctl -w fs.inotify.max_user_watches=524288
 
+# Generate missing locale
+sudo locale-gen en_US
+sudo locale-gen en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8
+
 # Disable apt update ubuntu pro esm warnings
 sudo [ -s "$HOME/.nvm/bash_completion" ] && rm /etc/apt/apt.conf.d/20apt-esm-hook.conf
 
