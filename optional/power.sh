417f81cf08caa7cdbdb490414c68bd9aa699c5ef
Geert Eltink <geert.eltink@gmail.com>
2023-10-10 13:09:39 +0200

    feat: add power tweaks

---
+++ optional/power.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -Eueo pipefail
+
+sudo add-apt-repository -y ppa:linrunner/tlp
+sudo apt -y update
+
+PACKAGES=(
+  thermald
+  tlp
+  tlp-rdw
+  powertop
+)
+
+for pkg in "${PACKAGES[@]}"; do
+  dpkg -s $pkg > /dev/null 2>&1 || sudo apt -y install $pkg
+done
+
+sudo powertop --auto-tune
+
+exit 0
