43e9b981f328264dc6fada7666a14c50afe6fd22
Geert Eltink <geert.eltink@gmail.com>
2023-11-05 12:18:53 +0100

    feat: remove time sync workaround
    It should be fixed in the latest kernel release in the store: https://devblogs.microsoft.com/commandline/servicing-the-windows-subsystem-for-linux-wsl-2-linux-kernel/#bug-fix-clock-sync

--- system/install.sh
+++ system/install.sh
@@ -1,18 +1,6 @@
 #!/usr/bin/env bash
 set -Eueo pipefail
 
-PACKAGES=(
-  ntp
-  ntpdate
-)
-
-for pkg in "${PACKAGES[@]}"; do
-  dpkg -s $pkg > /dev/null 2>&1 || sudo apt -y install $pkg
-done
-
-# Auto sync time
-sudo ntpdate -u pool.ntp.org
-
 # Use max file watches
 sudo sysctl -w fs.inotify.max_user_watches=524288
 
