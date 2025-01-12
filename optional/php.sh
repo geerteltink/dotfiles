f22ac23664e4a0c846fb9ae2878ce8c3edcdb727
Geert Eltink <geert.eltink@gmail.com>
2025-01-12 12:42:44 +0100

    feat: php 8.4

--- optional/php.sh
+++ optional/php.sh
@@ -12,10 +12,10 @@ if [ ! -f /etc/apt/sources.list.d/ondrej* ]; then
     sudo add-apt-repository -yu ppa:ondrej/php
 fi
 
-if [ ! -f /usr/bin/php8.3 ]
+if [ ! -f /usr/bin/php8.4 ]
 then
-    sudo apt -y install php8.3-{common,cli,bcmath,curl,gd,intl,mbstring,xml,mysql,ldap,redis,sqlite3,amqp}
-    sudo update-alternatives --set php /usr/bin/php8.3
+    sudo apt -y install php8.4-{common,cli,bcmath,curl,gd,intl,mbstring,xml,mysql,ldap,redis,sqlite3,amqp}
+    sudo update-alternatives --set php /usr/bin/php8.4
 fi
 
 if [ ! "$(which composer)" ]; then
