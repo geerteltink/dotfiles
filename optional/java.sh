20cf91b00bb56f9b1c6fb47492828ebf0a2eed1a
Geert Eltink <geert.eltink@gmail.com>
2025-06-27 09:11:28 +0200

    feat: use Azul Zulu JDK LTS

--- optional/java.sh
+++ optional/java.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -Eueo pipefail
 
-sudo apt install -y openjdk-21-jdk
+curl -s https://repos.azul.com/azul-repo.key | sudo gpg --dearmor --yes -o /usr/share/keyrings/azul.gpg
+echo "deb [signed-by=/usr/share/keyrings/azul.gpg] https://repos.azul.com/zulu/deb stable main" | sudo tee /etc/apt/sources.list.d/zulu.list
+sudo apt update
+
+sudo apt install -y zulu21-jdk
 
 exit 0
